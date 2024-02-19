66b267047d60de41ba5d93d375d78aeb42356354
Stacey Sanchez <staceysanchez27@gmail.com>
2024-02-19 17:15:05 -0500

    working on generate paranthesis

---
+++ stack/medium/generate_parenthesis.js
@@ -0,0 +1,32 @@
+// TODO: Work on understanding
+
+
+var generateParenthesis = (n) => dfs(n);
+
+const dfs = (n, combos = [], open = 0, close = 0, path = []) => {
+    const isBaseCase = (path.length === (n * 2));
+    if (isBaseCase) {
+        combos.push(path.join(''));
+        return combos;
+    }
+
+    const isOpen = open < n;
+    if (isOpen) backTrackOpen(n, combos, open, close, path);
+
+    const isClose = close < open;
+    if (isClose) backTrackClose(n, combos, open, close, path);
+
+    return combos;
+}
+
+const backTrackOpen = (n, combos, open, close, path) => {
+    path.push('(');
+    dfs(n, combos, (open + 1), close, path);
+    path.pop();
+}
+
+const backTrackClose = (n, combos, open, close, path) => {
+    path.push(')');
+    dfs(n, combos, open, (close + 1), path);
+    path.pop();
+}
