9418a73fd74450b89f9ddab06eef0810f9aad431
Stacey Sanchez <staceysanchez27@gmail.com>
2024-02-22 14:21:57 -0500

    koko eating bananas using binary search

---
+++ binarysearch/koko_eatingbananas.js
@@ -0,0 +1,71 @@
+// piles array: length represents number of piles of banana
+// elements themselves represent number of bananas per pile 
+// have to return the min speed needed to eat all the bananas before the guards return, represented in h hours 
+// if a pile has less bananas than the number eaten per hour, will just eat that pile amount for that hr 
+
+function minEatingSpeed(piles, h) {
+    let left = 1;                                           // the min speed the monkey can eat the bananas is at 1/hr
+    let right = Math.max(...piles);                         // the max speed is the largest amount of banana in one pile
+                                                            // will do binary search to find min speed needed to eat it all before guards arrive 
+    while(left<right) {
+        const mid = Math.floor((right+left)/2);             // mid speed between right and left 
+        
+        let hourSpent = 0;
+
+        for(let i=0; i<piles.length; i++) {                 // will iterate through the piles array and find the amount of hrs needed to consume bananas based on mid speed
+            hourSpent += Math.ceil(piles[i]/mid)            // ex: 10 bananas eaten at 5 bananas an hr (mid) will result in 2 hrs. 
+        }                                                   // Math.ceil rounds up 
+
+        if(hourSpent > h) {                                 // if hourspent is greater than the time the guard will come back (h), need to move up left pointer to the avg speed (mid) is higher
+            left = mid + 1;                                 // and bananas can be eaten in less time 
+        }
+
+        if(hourSpent <= h) {                                // since at hrs spent to eat the bananas at mid speed is at least equal if not less than h, can move right pointer to mid. 
+            right = mid;
+        }
+
+    }
+
+    return right; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var minEatingSpeed = function (piles, h) {
+    let [left, right] = [1, Math.max(...piles)];
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        let hourSpent = 0;
+
+        for (let i = 0; i < piles.length; i++) {
+            hourSpent += Math.ceil(piles[i] / mid);
+        }
+
+        const isTargetGreater = h < hourSpent;
+        if (isTargetGreater) left = mid + 1;
+
+        const isTargetLess = hourSpent <= h;
+        if (isTargetLess) right = mid;
+    }
+
+    return right;
+};
