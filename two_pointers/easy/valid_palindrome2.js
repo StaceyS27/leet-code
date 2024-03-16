bb0fd51fbbba196877ecdfaf7bd7bf912ca88f67
Stacey Sanchez <staceysanchez27@gmail.com>
2024-03-16 01:00:26 -0400

    valid palindrome II solved using 2 pointers

--- two_pointers/easy/valid_palindrome2.js
+++ two_pointers/easy/valid_palindrome2.js
@@ -1,52 +1,61 @@
 // Valid Palindrome II
 
-var validPalindrome = function(s) {
-    // make a hash and if the values has 3 characters or more with only 1 frequency 
-    // then false 
+// a palindrome is a word that is the same when written forwards and backwards
+// have to return true if word is a palindrome after up to one letter removal 
 
-    let left = 0;
-    let right = s.length - 1;
+// 2 pointers are used an if the characters don't match at the pointers
+// the string is sliced to create a new substring that only considers part of the string between L and R pointers
+    // dont need to consider the previous letters bc already proven to be a palindrome
+// 2 slices are created because want to consider if a palidrome will exist if either of the characters are removed
+    // in the problem, can remove up to one letter to make palindrome 
+// slice method makes new string starting and including the first argument 
+// and up to, but not including the second argument 
+    // skipL makes a string that would exist if l is removed (achieved by skipping it)
+        // going up to r+1 means that the character at r will be included 
+    // skipR makes a string that would exist is r is removed (achieved by forming string from l to r but not including r)
 
-    let count = 0;
+// the strings are passed into the isPalindrome helper function and if once again mismtaching letters are found (only 1 letter allowed for the problem),
+// the helper function will return false 
+// if when both strings skipL and skipR are passed in and both return false,
+// then OR statement will evaluate to false and the entire function will also return false 
+// if when the skipR or skipL are passed in and one returns true (if both are true, will also evaluate to true),
+    // would happen when no more mismatches are found
+    // the whole function will return true
+    // this is because with just the removal of either the char at l or r, the palidrome exists
 
-    while(left<=right){
-        if(s[left]!==s[right]){
-            count+=1
+function validPalindrome(s){
+    let l = 0;
+    let r = s.length - 1;
+
+    while(l < r){
+        if(s[l] !== s[r]){
+            const skipL = s.slice(l+1, r+1);
+            const skipR = s.slice(l, r);
+
+            return isPalindrome(skipL) || isPalindrome(skipR);
         }
-        left++;
-        right--;
+
+        l++;
+        r--;
     }
 
-    if(count > 1){
-        return false
-    } else {
-        return true 
-    }
-};
+    return true;                                // return true if loop finishes and never returns false
+}                                               // happens when all the characters of the string at the 2 pointers correspond and are the same 
 
 
-var validPalindrome = function(s) {
-    let map = new Map();
-    let count = 0;
+function isPalindrome(s) {
+    let l=0;
+    let r = s.length -1;
 
-    for(let i=0; i<s.length; i++){
-        if(!map.has(s[i])){
-            map.set(s[i], 1)
-        } else {
-            map.set(s[i], (map.get(s[i])+1))
+    while(l<r){
+        if(s[l] !== s[r]){
+            return false
         }
-    }
-    console.log(map)
 
-    for(let value of map.values()) {
-        if(value === 1) {
-            count+=1;
-        }
+        l++;
+        r--;
     }
 
-    if(count>2){
-        return false
-    } else {
-        return true 
-    }
-  }
+    return true; 
+}
+
