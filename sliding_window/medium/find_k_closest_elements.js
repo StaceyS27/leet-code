f03fdcd39097f939c973249651fe560710bc7ddc
Stacey Sanchez <staceysanchez27@gmail.com>
2024-03-18 12:12:06 -0400

    correct solution for finding k closest elements using sliding window approach

--- sliding_window/medium/find_k_closest_elements.js
+++ sliding_window/medium/find_k_closest_elements.js
@@ -1,74 +1,76 @@
-// **** Not correct solution, need to work on it **** 
+// Find K Closest Elements
+
+// given a sorted array arr and 2 integers k and x
+// return the k closest integers to x in the array
+// result should be in ascending order 
+
+// another point given: the values are closer to the target value if the absolute value of the difference is less
+// and if the difference is that same, the smaller number is considered closer 
+
+// tip: x may or may not be in the array 
+// if it is not in the array, need to find k numbers closest to it and return it 
+// if it is in the array, the number itself is considered part of the k numbers closest to x 
 
 
-
-var findClosestElements = function(arr, k, x) {
-    // result should be sorted in ascending order 
-    // binary search for x 
-    // once found put 2 pointers right before it and after it 
-    // if the abs value of the difference is greater than the other leave that pointer there
-    // move the other pointer until the diff is equal to the other side 
-    // make sure to consider the boundaries
-    // and if the window doesnt have k values in it 
-    // store diff in some sort of variable 
-
-    let set = new Set(arr);
-    if(!set.has(x)) {
-        let result =[];
-        if(x>arr[arr.length-1]){
-            for(let i=arr.length-1; i>=0 && result.length<k; i--){
-                result.push(arr[i])
-            }
-        } else {
-            for(let i=0; i<arr.length && result.length<k; i++) {
-                result.push(arr[i])
-            }
-        }
-
-        return result; 
-    }
-
-    // binary search
+var findClosestElements = function (arr, k, x) {
+    // Binary search to find index of x, which is the target
     let left = 0;
-    let right = arr.length-1;
-
+    let right = arr.length - 1;
     let targetIndex = -1;
 
-    while(left<=right) {
-        let mid = Math.floor((left+right)/2)
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
 
-        if(arr[mid] === x) {
+        if (arr[mid] === x) {                                   
             targetIndex = mid;
-            break; 
-        }
-        if(arr[mid]>x){
+            break;
+        } else if (arr[mid] < x) {
+            left = mid + 1;
+        } else {
             right = mid - 1;
-        } else {
-            left = mid + 1
         }
     }
 
-    console.log(targetIndex)
+    // If x is not found, find the closest match
+    let minDiff = Infinity                                          // x may not be in tha array so the targetIndex may still be unchanged
+    let closestTarget = null
+    if (targetIndex === -1) {
+        for (let i = 0; i < arr.length; i++) {
+            if (Math.abs(arr[i] - x) < minDiff) {
+                minDiff = Math.abs(arr[i] - x)
+                closestTarget = i
+            }
+        }
+    }
+    console.log(closestTarget)
 
-    let leftBorder = targetIndex;
+    let leftBorder = targetIndex;                                   // leave the borders on index in case desired k value/window size is only 1 
     let rightBorder = targetIndex;
-    
-    let windowSize = (rightBorder - leftBorder) + 1;
 
-    while(windowSize<k) {
-        if(leftBorder === 0) {
-            rightBorder+=1;
-        } else if (rightBorder === arr.length -1) {
-            leftBorder-=1
-        } else if((Math.abs(arr[leftBorder-1] - x)) <= (Math.abs(arr[rightBorder+1] - x))) {
-            leftBorder-=1
-        } else {
-            rightBorder+=1
-        }
+    // Update borders if closestTarget is found
+    if (closestTarget !== null) {                                   // will change borders if closestTarget is not null which happens if x is not in the array
+        leftBorder = closestTarget;                                 // need to specify closesttarget not equal null bc if it is equal to 0, still considered falsey
+        rightBorder = closestTarget;                                // and won't execute the if block 
     }
 
-    let output = arr.slice(leftBorder, rightBorder + 1)
+    let windowSize = rightBorder - leftBorder + 1;
 
-    return output
-    
+    while (windowSize < k) {
+        if (leftBorder === 0) {                                                                         // make sure borders won't go out of bounds 
+            rightBorder++;
+        } else if (rightBorder === arr.length - 1) {
+            leftBorder--;
+        } else if (Math.abs(arr[leftBorder - 1] - x) <= Math.abs(arr[rightBorder + 1] - x)) {           // as mentioned before, even if the diff is the same b/w x and two numbers, the smaller number is considered closest 
+            leftBorder--;                                                                               // so will decrement left border to include smaller number in k closest to element
+        } else {
+            rightBorder++;
+        }
+
+        // Update windowSize after adjusting borders
+        windowSize = rightBorder - leftBorder + 1;                                                  // need to recalculate window size inside loop once borders are changed
+    }                                                                                               // won't do it from the outside where window size is initialized
+
+    let output = arr.slice(leftBorder, rightBorder + 1)                                             // ensures k closest remains in ascending order 
+                                                                                                    // 2nd argument in slice not included in newly formed array
+    return output;
 };
