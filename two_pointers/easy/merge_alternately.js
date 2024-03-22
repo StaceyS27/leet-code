c973d0bc0d7eb4f4c103220fa34f4bc8319795fc
Stacey Sanchez <staceysanchez27@gmail.com>
2024-03-22 00:35:49 -0400

    time and space complexity for merge strings alternately using 2 pointers

--- two_pointers/easy/merge_alternately.js
+++ two_pointers/easy/merge_alternately.js
@@ -5,18 +5,18 @@
 // if of different lengths, append the remaining letters onto the end of the merged string 
 
 function mergeAlternately(word1, word2) {
-    let word1P = 0;
+    let word1P = 0;                                                     // setting pointers on word1 and word2
     let word2P = 0;
 
-    let mergedStr = ""
+    let mergedStr = ""                                                
 
-    while (word1P < word1.length && word2P < word2.length) {
-        mergedStr += word1[word1P];
-        mergedStr += word2[word2P];
-
-        word1P++;
-        word2P++;
-    }
+    while (word1P < word1.length && word2P < word2.length) {            // while both pointers are still not past the last index for either strings,
+        mergedStr += word1[word1P];                                     // can add each character alternately and move both pointers
+        mergedStr += word2[word2P];                                     // however, when the either of the pointers are passed the last index, will exit while loop
+                                                                        // both statements in while loop have to be true to continue 
+        word1P++;                                                       // if not, will exit and which ever string has not been fully iterated through 
+        word2P++;                                                       // will be appended to mergedStr using either for loops below                                                 
+    }                                                                   // this scenerio will happen when the strings are not of the same length             
 
     if (word1P !== word1.length) {
         for (let i = word1P; i < word1.length; i++) {
@@ -30,5 +30,15 @@ function mergeAlternately(word1, word2) {
         }
     }
 
-    return mergedStr
+    return mergedStr;
 }
+
+// time complexity - O(max(m, n))
+    // this expression above says that the time complexity is Big O of the longer string (lengths of word1 and 2 represented by m and n)
+    // this is the case because the 2 strings are iterated thru at the same time 
+    // and then if there is a longer string, the iteration continues from where the pointer left off until its iterated thru completely 
+        // so not 0(n+m) because its happening at one time ?
+
+// space complexity - O(n + m)
+    // where n amd m are the lengths of word1 and word2 respectively 
+    // the space occupied by the mergedStr variable is directly related to the summation of the lengths of both word1 and 2 
