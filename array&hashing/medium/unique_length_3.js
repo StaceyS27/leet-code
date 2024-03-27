62760f1ecbfef6dcf8376dfd5a44e07b519074b9
Stacey Sanchez <staceysanchez27@gmail.com>
2024-03-27 00:43:06 -0400

    O(n^3) time complexity does not pass all test cases

--- array&hashing/medium/unique_length_3.js
+++ array&hashing/medium/unique_length_3.js
@@ -1,3 +1,27 @@
 // 1930 Unique Length-3 Palindromic Subsequences 
 
-// working on solution 
+
+
+// only passes 43/70 time complexity O(n^3)
+var countPalindromicSubsequence = function(s) {
+    let set = new Set();
+    let left = 0;
+
+    while (left < s.length - 2) {
+        let middle = left + 1;
+        while (middle < s.length - 1) {
+            let right = middle + 1;
+            while (right < s.length) {
+                if (s[left] === s[right]) {
+                    let palindrome = s[left] + s[middle] + s[right];
+                    set.add(palindrome);
+                }
+                right++;
+            }
+            middle++;
+        }
+        left++;
+    }
+
+    return set.size;
+};
